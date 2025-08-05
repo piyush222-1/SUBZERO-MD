4ddbd4f8aba5dc63778acb98af248da43fd5d495
piyush222-1 <authenticcation2@gmail.com>
2025-08-05 18:45:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;CIVhwbqa#siVceJDQQqjNL0TN7zknGmH1nsrS2R3Xzxn7qwLd_BU", // Put Session Id Here Inside "*"
+  SESSION_ID: "
+    SUBZERO-MD;;;7QlWwBpK#n9lpGdlkrgZnEFUogJTtK6rYud3i37KrazgSpDcyH1c", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
